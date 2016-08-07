6a59141a5d85824fa90019955bab5c5df768918a
Mate Retsagi <retsagimate@gmail.com>
2016-08-07 15:33:11 +0200

    webshop sorting added

--- webshop/index.php
+++ webshop/index.php
@@ -2,12 +2,49 @@
 
 require 'boltszkriptek/kapcsolat_a_mysqlhez.php';
 
-$sql = mysqli_query($dblink, "SELECT `id`, `product_name`, `price` from products ORDER BY `product_name` ASC") or die(mysqli_error($dblink));
+$output = '<form method="post">'
+    //.'<label for="order">Rendezés</label><br/>'
+    .'<select name="order" id="order">'
+    .'<option value="0">---Válasszon rendezési módot---</option>'
+    .'<option value="1">Név szerint növekvő</option>'
+    .'<option value="2">Név szerint csökkenő</option>'
+    .'<option value="3">Ár szerint növekvő</option>'
+    .'<option value="4">Ár szerint csökkenő</option>'
+    .'</select>'
+    .'<input type="submit" name="submit" value="Rendez">'
+    .'</form>'
+    .'<br/>';
+
+if(filter_input(INPUT_POST, 'submit')) {
+    $orderMethod = filter_input(INPUT_POST, 'order');
+
+    switch ($orderMethod) {
+        case '1' :
+            $sql = mysqli_query($dblink, "SELECT `id`, `product_name`, `price` from products ORDER BY `product_name` ASC") or die(mysqli_error($dblink));
+            break;
+        case '2' :
+            $sql = mysqli_query($dblink, "SELECT `id`, `product_name`, `price` from products ORDER BY `product_name` DESC") or die(mysqli_error($dblink));
+            break;
+        case '3' :
+            $sql = mysqli_query($dblink, "SELECT `id`, `product_name`, `price` from products ORDER BY `price` ASC") or die(mysqli_error($dblink));
+            break;
+        case '4' :
+            $sql = mysqli_query($dblink, "SELECT `id`, `product_name`, `price` from products ORDER BY `price` DESC") or die(mysqli_error($dblink));
+            break;
+        default :
+            $sql = mysqli_query($dblink, "SELECT `id`, `product_name`, `price` from products ORDER BY `product_name` ASC") or die(mysqli_error($dblink));
+    }
+} else {
+    $sql = mysqli_query($dblink, "SELECT `id`, `product_name`, `price` from products ORDER BY `product_name` ASC") or die(mysqli_error($dblink));
+}
+
+
+//$sql = mysqli_query($dblink, "SELECT `id`, `product_name`, `price` from products ORDER BY `product_name` ASC") or die(mysqli_error($dblink));
 
 $numberOfRows = mysqli_num_rows($sql);
 
 if ($numberOfRows > 0) {
-    $output = '<table border="1" cellspacing="0" cellpadding="0" style="text-align: center;">'
+    $output .= '<table border="1" cellspacing="0" cellpadding="0" style="text-align: center;">'
         . '<tr>'
         . '<th>Termék neve</th>'
         . '<th>Termék ára</th>'
@@ -78,6 +115,12 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <!--
+        keep selected value in drop-down after submission - not yet working
+        <script type="text/javascript">
+            document.getElementById('order').value = "<?php// echo $_POST['order'];?>";
+        </script>
+        -->
     </head>
     <body>
         <?php
