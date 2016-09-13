ca0b7de883ea9b1144826d97592c4d2541a066b6
szembeszomszed <retsagimate@gmail.com>
2016-09-13 12:59:34 +0200

    címkezelő osztály létrehozása step 1: első üres objektum létrehozása

--- demo.php
+++ demo.php
@@ -1,3 +1,13 @@
 <?php
-echo 'this is a test page';
+
+//címkezelő osztály betöltése
+require ('class.Address.inc');
+
+echo '<h2>Címkezelő osztály példányosítása (üres példány)</h2>';
+
+// osztály megszólítása
+$address = new Address;
+
+// a var_export a true-val stringként tér vissza, és nem void lesz
+echo '<pre>'.  var_export($address, true).'</pre>';
 
