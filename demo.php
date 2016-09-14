7a9dee564b804028d6235676e9363046fd31685f
szembeszomszed <retsagimate@gmail.com>
2016-09-14 11:08:39 +0200

    __get és __set alkalmazása

--- demo.php
+++ demo.php
@@ -17,8 +17,9 @@ echo '<h2>Objektumtulajdonságok "feltöltése" </h2>';
 $address->street_address_1 = "Frangepán utca 23.";
 $address->city_name = "Budapest";
 $address->subdivision_name = "Angyalföld";
-$address->postal_code = 1138;
+//$address->postal_code = 1138;
 $address->country_name = "Magyarország";
+$address->valami="bármi";
 echo '<pre>'.  var_export($address, true).'</pre>';
 
 echo '<h2>Cim kiírása az osztályban deklarált display metódussal</h2>';
