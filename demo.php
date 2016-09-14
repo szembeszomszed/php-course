dcd37caa8a5656be943303628c34a2264437ff3a
szembeszomszed <retsagimate@gmail.com>
2016-09-14 09:46:37 +0200

    objektum metódus (display) és használata

--- demo.php
+++ demo.php
@@ -1,6 +1,6 @@
 <?php
 
-//címkezelő osztály betöltése
+// címkezelő osztály betöltése
 require ('class.Address.inc');
 
 echo '<h2>Címkezelő osztály példányosítása (üres példány)</h2>';
@@ -11,3 +11,15 @@ $address = new Address;
 // a var_export a true-val stringként tér vissza, és nem void lesz
 echo '<pre>'.  var_export($address, true).'</pre>';
 
+echo '<h2>Objektumtulajdonságok "feltöltése" </h2>';
+
+// obj tulajdonsága (property) a nyíl után, majd értékadás
+$address->street_address_1 = "Frangepán utca 23.";
+$address->city_name = "Budapest";
+$address->subdivision_name = "Angyalföld";
+$address->postal_code = 1138;
+$address->country_name = "Magyarország";
+echo '<pre>'.  var_export($address, true).'</pre>';
+
+echo '<h2>Cim kiírása az osztályban deklarált display metódussal</h2>';
+echo $address->display();
--- readme.md
+++ readme.md
@@ -5,3 +5,5 @@ PHP Haladó Tanfolyam - Ruander Oktatóközpont
 2.a GIT alapok, bitbucket, ssh kulcsok használata
 
 2.b OOP alapok, osztály létrehozása, példány származtatása, tulajdonságok - metódusok
+
+3. vhost és host beállítása lokális eléréshez http://local.oop url-en. objektumtulajdonságok feltöltése, publikus metódus létrehozása és használata
