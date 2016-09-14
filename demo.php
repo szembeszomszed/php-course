582ff5a4d7ab33456c0efddadb584249f77c3e9f
szembeszomszed <retsagimate@gmail.com>
2016-09-14 11:48:11 +0200

    objektum létrehozása tömbmegadással a konstruktor segítségével

--- demo.php
+++ demo.php
@@ -19,8 +19,21 @@ $address->city_name = "Budapest";
 $address->subdivision_name = "Angyalföld";
 //$address->postal_code = 1138;
 $address->country_name = "Magyarország";
-$address->valami="bármi";
+//$address->valami="bármi";
 echo '<pre>'.  var_export($address, true).'</pre>';
 
-echo '<h2>Cim kiírása az osztályban deklarált display metódussal</h2>';
+echo '<h2>Cím kiírása az osztályban deklarált display metódussal</h2>';
 echo $address->display();
+
+echo '<h2>Objektum létrehozása tömbből konstruktor segítségével</h2>';
+$data = [
+    'street_address_1'=>'Kossuth tér 2.',
+    'city_name'=>'Paks',
+    'country_name'=>'Magyarország',    
+];
+
+$address_2 = new Address($data);
+echo $address_2->display();
+echo '<pre>'.var_export($address_2, true).'</pre>';
+
+
